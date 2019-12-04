415c5e3dc7f8f89215239a37f008f8ef296259b1
RST001 <ricardosteruya@gmail.com>
2019-12-03 21:11:38 -0300

    add comentarios e limpeza de cod

--- anel4_ospf.cc
+++ anel4_ospf.cc
@@ -23,35 +23,32 @@ using namespace ns3;
 
 int main (int argc, char** argv)
 {
+    //Criando conjunto de nos
     NodeContainer c;
     c.Create(4);
-
+    //Agrupando nos para instalacao de dispositivos e canal de rede
     NodeContainer n0n1 = NodeContainer( c.Get(0), c.Get(1) );
     NodeContainer n1n2 = NodeContainer( c.Get(1), c.Get(2) );
     NodeContainer n2n3 = NodeContainer( c.Get(2), c.Get(3) );
     NodeContainer n3n0 = NodeContainer( c.Get(3), c.Get(0) );
 
     //Iniciando links sem ip
-    //NS_LOG_INFO ("Create channels.");
     PointToPointHelper p2p;
+    //Padroes do enlace
     p2p.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
     p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
-
+    //Criando sub-redes
     NetDeviceContainer d0d1 = p2p.Install( n0n1 );
     NetDeviceContainer d1d2 = p2p.Install( n1n2 );
     NetDeviceContainer d2d3 = p2p.Install( n2n3 );
     NetDeviceContainer d3d0 = p2p.Install( n3n0 );
 
     Ipv4GlobalRoutingHelper routingHelper;
-    //Ipv4ListRoutingHelper listRH;
-    //listRH.Add (routingHelper, 0);
     //Instalando pilhas de protocolos/aplicacoes nos nos
     InternetStackHelper internet;
-    //internet.AddRoutingProtocol(routingHelper, 0);
     internet.Install (c); 
     
-    //ipv4
-    //NS_LOG_INFO ("Assign IP Addresses.");
+    //Adicionando enderecos ipv4
     Ipv4AddressHelper ipv4;
 
     ipv4.SetBase ("10.0.0.0", "255.255.255.0");
@@ -69,13 +66,11 @@ int main (int argc, char** argv)
      
     //Criando tabelas de roteamento via ospf
     routingHelper.PopulateRoutingTables();
-    
-      
+          
+    //Criando estruturas para print de tabelas de roteamento    
     Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> (&std::cout);
 
     Ipv4ListRoutingHelper::PrintRoutingTableAllAt (Seconds (1.0), routingStream);	
-    //Time u = new Time("s");
-    //Ipv4ListRouting::PrintRoutingTable (routingStream, new Time("s").Resolution());
 
     Simulator::Stop (Seconds (10.0));
     auto start = high_resolution_clock::now();
@@ -84,35 +79,5 @@ int main (int argc, char** argv)
 	auto duration = duration_cast<nanoseconds>(stop - start);
 	cout << duration.count() << " nanoseconds" << endl;
 
-    Simulator::Destroy ();
-    //Print de tabela de roteamento para no 0
-	/*
-	Ptr<Ipv4GlobalRouting> routing = 0;
-    Ipv4GlobalRoutingHelper routingHelper;
-    Ptr<Ipv4> i4 = c.Get (0) -> GetObject<Ipv4> ();
-    std::cout << i4.GetRoutingProtocol() << std::endl;
-*/
-    //Ipv4RoutingTableEntry route = ;
-  //  uint32_t nbRoutes = 0;
-    //routing = routingHelper;
-    /*
-    std::cout << "Routing table of " << c.Get (0) << " : " << std::endl;
-    std::cout << "Destination\t\t" << "Gateway\t\t" << "Interface\t" <<  "Prefix to use" << std::endl;
-    nbRoutes = routing->GetNRoutes ();
-    for (uint32_t i = 0; i < nbRoutes; i++)
-      {
-        route = routing->GetRoute(i);
-        std::cout << route.GetDest () << "\t"
-                  << route.GetGateway () << "\t"
-                  << route.GetInterface () << "\t"
-                  << std::endl;
-      }
-    */
-    //Time::Unit unit = Time::Unit();
-    //std::ofstream ofs;
-    //ofs.open ("test.txt", std::ofstream::out);
-    //Ptr<OutputStreamWrapper> stream = OutputStreamWrapper(ofs).GetStream();		
-	//Ipv4GlobalRouting::PrintRoutingTable(stream, Time::MS);
-    //ofs.close();
-     
+    Simulator::Destroy ();    
 }
--- anel4_rip.cc
+++ anel4_rip.cc
@@ -23,41 +23,40 @@ using namespace ns3;
 
 int main (int argc, char** argv)
 {
+    //Criando conjunto de nos
     NodeContainer c;
     c.Create(4);
-
+    //Agrupando nos para instalacao de dispositivos e canal de rede
     NodeContainer n0n1 = NodeContainer( c.Get(0), c.Get(1) );
     NodeContainer n1n2 = NodeContainer( c.Get(1), c.Get(2) );
     NodeContainer n2n3 = NodeContainer( c.Get(2), c.Get(3) );
     NodeContainer n3n0 = NodeContainer( c.Get(3), c.Get(0) );
 
     //Iniciando links sem ip
-    //NS_LOG_INFO ("Create channels.");
     PointToPointHelper p2p;
+    //Padroes do enlace
     p2p.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
     p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
-
+    //Criando sub-redes
     NetDeviceContainer d0d1 = p2p.Install( n0n1 );
     NetDeviceContainer d1d2 = p2p.Install( n1n2 );
     NetDeviceContainer d2d3 = p2p.Install( n2n3 );
     NetDeviceContainer d3d0 = p2p.Install( n3n0 );  
     
-    //Instalando pilhas de protocolos/aplicacoes nos nos
+    //Instalando pilhas de protocolos/aplicacoes em nos
     InternetStackHelper internet;
-
-    RipHelper ripRouting;
     
+    //Iniciando configuracao do RIP
+    RipHelper ripRouting;    
     Ipv4ListRoutingHelper listRH;
     listRH.Add (ripRouting, 0);
-    internet.SetRoutingHelper (listRH);
-     
+    //ajustando instalacao para protocolos usarem RIP
+    internet.SetRoutingHelper (listRH);     
      
     //Criando tabelas de roteamento via rip
-    internet.Install (c);
-        
+    internet.Install (c);        
     
-    //ipv4
-    //Assign IP Addresses
+    //aplicando enderecos ipv4
     Ipv4AddressHelper ipv4;
 
     ipv4.SetBase ("10.0.0.0", "255.255.255.0");
